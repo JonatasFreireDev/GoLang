c4c8144edfbca6b5ac6817d865d51aafbd901df3
Jonatas <jonatasf1997@hotmail.com>
2022-09-19 00:40:53 -0300

    feat: get ip infos

---
+++ go-server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil{
+		fmt.Fprintf(w, "parseform() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request succec")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "name = %s\n", name)
+	fmt.Fprintf(w, "address = %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprint(w, "Hellow!")
+}
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("starting server at port 8080 \n")
+
+	if err:= http.ListenAndServe(":8080", nil); err !=nil{
+		log.Fatal(err)
+	}
+}
