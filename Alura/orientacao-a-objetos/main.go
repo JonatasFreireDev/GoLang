8c67d97f9226abc1a70006ae79cd5700463669db
Jonatas Freire <Jonatasf1997@hotmail.com>
2023-01-24 16:52:02 -0300

    feat: whays to instance an struct

--- Alura/orientacao-a-objetos/main.go
+++ Alura/orientacao-a-objetos/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func main() {
+	guilherme := ContaCorrente{
+		titular:       "teste",
+		numeroAgencia: 10,
+		numeroConta:   12,
+		saldo:         1231,
+	}
+
+	bruna := ContaCorrente{
+		"teste",
+		10,
+		12,
+		1231,
+	}
+
+	rogerio := ContaCorrente{"Salve", 10, 10, 10}
+
+	fmt.Println(guilherme)
+	fmt.Println(bruna)
+	fmt.Println(rogerio)
 }
