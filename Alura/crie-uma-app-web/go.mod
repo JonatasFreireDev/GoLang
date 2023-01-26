12cc4c1ed9d4e39a8501ab854a52fc2a3e016391
Jonatas Freire <Jonatasf1997@hotmail.com>
2023-01-26 10:51:17 -0300

    banco de dados

---
+++ Alura/crie-uma-app-web/go.mod
@@ -0,0 +1,5 @@
+module crie-um-app-web
+
+go 1.19
+
+require github.com/lib/pq v1.10.7 // indirect
--- Alura/crie-uma-app-web/main.go
+++ Alura/crie-uma-app-web/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -19,14 +24,58 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{"Camiseta", "Azul", 49, 10},
-		{"Teclado", "RGB", 799, 49},
-		{"Teclado", "RGB", 799, 49},
-		{"Teclado", "RGB", 799, 49},
-		{"Teclado", "RGB", 799, 49},
+func conectaComBD() *sql.DB {
+	conexao := "user=postgres dbname=loja password=123456 host=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	return db
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	db := conectaComBD()
+	defer db.Close()
+
+	selectAllProducts, err := db.Query("SELECT * FROM produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectAllProducts.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectAllProducts.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+
+	// produtos := []Produto{
+	// 	{"Camiseta", "Azul", 49, 10},
+	// 	{"Teclado", "RGB", 799, 49},
+	// 	{"Teclado", "RGB", 799, 49},
+	// 	{"Teclado", "RGB", 799, 49},
+	// 	{"Teclado", "RGB", 799, 49},
+	// }
 
 	temp.ExecuteTemplate(w, "Index", produtos)
 }
