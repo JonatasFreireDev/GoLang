c39ed04ac4c3c8867f1991cefe65e7e891892221
Jonatas Freire <jonatasf1997@hotmail.com>
2023-01-27 01:44:41 -0300

    middlewares e cors

--- Alura/api-go-rest/routes/routes.go
+++ Alura/api-go-rest/routes/routes.go
@@ -2,19 +2,23 @@ package routes
 
 import (
 	"api-go-rest/controllers"
+	"api-go-rest/middleware"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
+
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("GET")
 	r.HandleFunc("/api/personalidades", controllers.PostPersonalidade).Methods("POST")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletePersonalidade).Methods("DELETE")
 	r.HandleFunc("/api/personalidades/{id}", controllers.PutPersonalidade).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
